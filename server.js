a4a8fdbd4e856335da412d38041b2ef8fbfdb39b
Final Bosss <68625695+tony-dark-tony@users.noreply.github.com>
2021-07-19 20:21:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const axios = require('axios');
 app.use(express.json())
 app.get("/", (req, res)=>{
-    axios.get("https://api.telegram.org/bot1937895124:AAGREoiVTtv8m22nGVoT68tsL81MxI9Ah1M/sendMessage?chat_id=-546716562&text=C%C3%B3%20m%C6%B0a")
+    axios.get("https://api.telegram.org/bot1937895124:AAGREoiVTtv8m22nGVoT68tsL81MxI9Ah1M/sendMessage?chat_id=-546716562&text=C%C3%B3%20m%C6%B0a");
+    console.log("Requested")
 })
 app.listen(1212);
